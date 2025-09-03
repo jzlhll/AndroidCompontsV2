9cdff2244eee42b7d05a046cb7c4aac6456e2539
allan.jiang <allan.jiang@typhur.com>
2025-09-03 15:29:25 +0800

    Edit

---
+++ app/src/main/java/com/allan/androidlearning/activities2/WebViewEditFragment.kt
@@ -0,0 +1,19 @@
+package com.allan.androidlearning.activities2
+
+import android.os.Bundle
+import com.allan.androidlearning.databinding.ActivityJsHtmlBinding
+import com.allan.classnameanno.EntryFrgName
+import com.au.module_android.ui.bindings.BindingFragment
+
+/**
+ * @author allan
+ * @date :2024/12/5 9:40
+ * @description:
+ */
+@EntryFrgName()
+class WebViewEditFragment : BindingFragment<ActivityJsHtmlBinding>() {
+
+    override fun onBindingCreated(savedInstanceState: Bundle?) {
+        binding.webView.loadUrl("file:///android_asset/webEdit.html")
+    }
+}
