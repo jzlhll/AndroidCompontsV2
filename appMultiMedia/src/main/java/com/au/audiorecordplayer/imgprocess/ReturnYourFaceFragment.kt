3e3da4cc4edc570763179fe9c277cce68f39dc57
allan.jiang <allan.jiang@typhur.com>
2025-09-04 17:22:14 +0800

    compose demo

---
+++ appMultiMedia/src/main/java/com/au/audiorecordplayer/imgprocess/ReturnYourFaceFragment.kt
@@ -0,0 +1,6 @@
+package com.au.audiorecordplayer.imgprocess
+
+import com.au.module_android.ui.bindings.BindingFragment
+
+class ReturnYourFaceFragment : BindingFragment() {
+}
---
+++ appcompose/src/main/java/com/au/appcompose/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.au.appcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import com.au.appcompose.ui.theme.AndroidCompontsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidCompontsTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.background(
+            color = Color(android.graphics.Color.GREEN),
+            shape = RoundedCornerShape(Dp(4f)) // 需要导入androidx.compose.foundation.shape包
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Hello1 $name!",
+            modifier = modifier
+        )
+        Text("Hello2 $name!")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Greeting("Changed")
+}
