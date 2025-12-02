75837276c59f763917e46e2fb775a03bf90b5de4
allan.jiang <jzl.hll@163.com>
2025-12-03 01:00:59 +0800

    update my navigation

--- Module-Android/src/main/java/com/au/module_android/ui/navigation/FragmentNavigationViewModel.kt
+++ Module-Android/src/main/java/com/au/module_android/ui/navigation/FragmentNavigationViewModel.kt
@@ -8,23 +8,30 @@ import com.au.module_android.simplelivedata.NoStickLiveData
 class FragmentNavigationViewModel : ViewModel() {
     private var dataListLiveData = mutableMapOf<String, NoStickLiveData<Bundle>>()
 
+    fun initAllPages(pageIds:List<String>) {
+        for (pageId in pageIds) {
+            val dataMap = NoStickLiveData<Bundle>()
+            dataListLiveData[pageId] = dataMap
+        }
+    }
+
     fun savePageData(pageId:String, bundle:Bundle) {
-        dataMap[pageId] = bundle
+        val dataMap = dataListLiveData.getOrPut(pageId) {
+            NoStickLiveData()
+        }
+        dataMap.setValueSafe(bundle)
     }
 
     fun getPageData(pageId:String):Bundle? {
-        return dataMap[pageId]
+        return dataListLiveData[pageId]?.value
     }
 
     fun updatePageData(pageId:String, kv:Map<String, Any?>) {
-        var bundle = dataMap[pageId]
-        if (bundle == null) {
-            bundle = Bundle()
-            dataMap[pageId] = bundle
-        }
+        val bundle = getPageData(pageId) ?: Bundle()
         kv.forEach { (k, v) ->
             if(v != null) bundle.putAny(k, v)
             else bundle.remove(k)
         }
+        savePageData(pageId, bundle)
     }
 }
