7c57d170ce16b82bbbd7a35ce2e31f3386bbaa8e
allan.jiang <jzl.hll@163.com>
2026-01-08 15:49:59 +0800

    compile version

--- Module-AuSimplePermission/build.gradle.kts
+++ Module-AuSimplePermission/build.gradle.kts
@@ -40,7 +40,7 @@ mavenPublishing {
 }
 
 mavenPublishing {
-    coordinates("io.github.jzlhll", "module-ausimplepermission", "0.0.2")
+    coordinates("io.github.jzlhll", "module-ausimplepermission", "0.0.4")
 
     pom {
         name = "module-ausimplepermission"
