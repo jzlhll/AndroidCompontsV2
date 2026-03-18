c38d11294cef38a2b395e91ba0c91f0c3006775a
allan.jiang <jzl.hll@163.com>
2026-03-18 21:26:51 +0800

    update unzip helper

--- Module-AndroidCommon/src/main/java/com/au/module_android/utils/UnzipHelper.kt
+++ Module-AndroidCommon/src/main/java/com/au/module_android/utils/UnzipHelper.kt
@@ -166,52 +166,19 @@ class UnzipHelper {
         } else {
             assetPath.trimEnd('/') + "/" + assetName
         }
-        var assetSize:Long
+        var assetSize:Long = -1L
         try {
-            val afd = assetManager.openFd(assetFilePath)
-            assetSize = afd.length
-            afd.close()
+            assetManager.open(assetFilePath).use {
+                assetSize = it.available().toLong()
+            }
         } catch (_: IOException) {
-            assetSize = -1L
         }
-        val same = assetSize >= 0 && targetFile.length() == assetSize
+        val targetSize = targetFile.length()
+        val same = assetSize >= 0 && targetSize == assetSize
         if (assetSize >= 0 && !same) {
             targetFile.delete()
         }
         return same
-    }
-
-    /**
-     * 检查assets目录下的文件是否与目标目录下的文件大小一致
-     */
-    fun checkSizeSame(assetManager: AssetManager, assetPath:String = "", assetNames: Array<String>, targetDir:String) : List<String> {
-        val notSameList = mutableListOf<String>()
-        for (i in assetNames.indices) {
-            val item = assetNames[i]
-            val targetFile = File(targetDir, item)
-            if (!targetFile.exists()) {
-                notSameList.add(item)
-                continue
-            }
-            val assetFilePath = if (assetPath.isBlank()) {
-                item
-            } else {
-                assetPath.trimEnd('/') + "/" + item
-            }
-            var assetSize:Long
-            try {
-                val afd = assetManager.openFd(assetFilePath)
-                assetSize = afd.length
-                afd.close()
-            } catch (_: IOException) {
-                assetSize = -1L
-            }
-            val same = assetSize >= 0 && targetFile.length() == assetSize
-            if (!same) {
-                notSameList.add(item)
-            }
-        }
-        return notSameList
     }
 
     /**
--- Module-AndroidCommon/src/main/java/com/au/module_android/utilthread/CoroutineConcurrentLimiter.kt
+++ Module-AndroidCommon/src/main/java/com/au/module_android/utilthread/CoroutineConcurrentLimiter.kt
@@ -1,9 +1,3 @@
-/* Created on 2026/03/11.
-* Copyright (C) 2026 @jzlhll.  All rights reserved.
-*
-* Licensed under the MIT License.
-* See LICENSE file in the project root for full license information.
-*/
 package com.au.module_android.utilthread
 
 import kotlinx.coroutines.*
